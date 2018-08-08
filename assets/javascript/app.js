d424dc8dcb64d6c42df55dcc0d4bfedf827fbd74
Reeve Johnson <reevewjohnson@gmail.com>
2018-08-07 20:57:17 -0500

    created variables for search terms

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,17 @@
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+$("#run-click").on(click, function() {
+    var searchTerm = $("#search-term").val().trim();
+    var numberOfResults = $("#num-records-select").val();
+    var beginDate = $("#start-year").val() + '0101';
+    var endDate = $("#end-year").val() + '1231';
+});
+
 url += '?' + $.param({
   'api-key': "7fc8dde8cc494ce2b5befb8da8215c04",
-  'q': "#",
-  'begin_date': "#",
-  'end_date': "#"
+  'q': searchTerm,
+  'begin_date': beginDate,
+  'end_date': endDate
 });
 
 $.ajax({
