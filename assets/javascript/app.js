70b3da3d488b23f75d77a86e534a5a0bc40b821f
Reeve Johnson <reevewjohnson@gmail.com>
2018-08-07 21:11:25 -0500

    created functioning on-click event for search button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,29 @@
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 
-$("#run-click").on(click, function() {
-    var searchTerm = $("#search-term").val().trim();
-    var numberOfResults = $("#num-records-select").val();
-    var beginDate = $("#start-year").val() + '0101';
-    var endDate = $("#end-year").val() + '1231';
-});
+var searchTerm;
+var numberOfResults;
+var beginDate;
+var endDate;
 
-url += '?' + $.param({
-  'api-key': "7fc8dde8cc494ce2b5befb8da8215c04",
-  'q': searchTerm,
-  'begin_date': beginDate,
-  'end_date': endDate
-});
+$("#run-search").on("click", function() {
+    event.preventDefault();
 
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
+    searchTerm = $("#search-term").val().trim();
+    numberOfResults = $("#num-records-select").val();
+    beginDate = $("#start-year").val() + '0101';
+    endDate = $("#end-year").val() + '1231';
+    url += '?' + $.param({
+        'api-key': "7fc8dde8cc494ce2b5befb8da8215c04",
+        'q': searchTerm,
+        'begin_date': beginDate,
+        'end_date': endDate
+    });
+    $.ajax({
+        url: url,
+        method: 'GET',
+      }).done(function(result) {
+        console.log(result);
+      }).fail(function(err) {
+        throw err;
+      });
 });
